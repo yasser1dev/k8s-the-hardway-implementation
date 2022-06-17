ee2cb883b81b969c6a35efa1cc485573a6df43cd
yasser1dev <yassirchihab1@gmail.com>
2022-06-18 00:03:42 +0100

    add nlb module config

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ module "k8s_vpc_config" {
   subnet_cidr_block = var.subnet_cidr_block
   K8s_sg_rules = var.security_group_rules
 }
+
+module "k8s_nlb_config" {
+  source = "./k8S-nlb"
+  subnet_id = module.k8s_vpc_config.subnet_id
+  vpc_id = module.k8s_vpc_config.vpc_id
+  target_ips = ["10.0.1.10","10.0.1.11","10.0.1.12"]
+}
