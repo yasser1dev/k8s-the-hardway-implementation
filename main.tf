1a780c95495080d3757200d8276b1ac9a75f9382
yasser1dev <yassirchihab1@gmail.com>
2022-06-21 00:44:16 +0100

    add first controller instance

--- main.tf
+++ main.tf
@@ -11,3 +11,30 @@ module "k8s_nlb_config" {
   vpc_id = module.k8s_vpc_config.vpc_id
   target_ips = ["10.0.1.10","10.0.1.11","10.0.1.12"]
 }
+
+module "k8s-nodes-cp1"{
+  source = "./k8s-nodes"
+  aws_instance_name = "controller-1"
+  aws_instance_ami = var.aws_instance_ami 
+  aws_instance_type    =  var.aws_instance_type
+  aws_instance_security_groups_ids  = ["${module.k8s_vpc_config.sg_id}"] 
+  aws_instance_subnet_id  = "${module.k8s_vpc_config.subnet_id}"
+  private_ip = "10.0.1.11"
+  user_data = "name=controller-1"
+  aws_instance_key_name = var.aws_instance_key_name
+  aws_instance_public_key  = var.aws_instance_public_key
+  aws_instance_tags = {
+    "Name" : "controller-1"
+  } 
+}
+
+module "ebs-volume" {
+  source = "./aws_volume_attachment"
+  instance_id = "${module.k8s-nodes-cp1.instance_id}"
+  availability_zone = var.availability_zone
+  device_name = var.device_name
+  size = var.size
+  ebs_volume_name = "ebs-cp-1"
+  ebs_type = var.ebs_type
+  tags = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -52,3 +52,55 @@ variable "security_group_rules" {
   ]
 }
 
+variable "aws_instance_ami" {
+  type        = string
+  default     = "ami-0905a1bd36fefbc75"
+}
+
+variable aws_instance_type {
+  type        = string
+  default     = "t3.micro"
+}
+
+variable aws_instance_key_name {
+  type        = string
+  default     = "kubernetes"
+}
+
+variable aws_instance_public_key {
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/5xRclmdC08MGDnX9WkV0f6aPw3xB9XwN15bNTrCAXNyA4M1CqeVJv/QPBBW7LMuJgdcBceVxOR5u/xWLAvq5mZZmWlHxKFHex6zCc6yuIuSGqKUnwKUtuCWuOJX+9tH2J5rarbh+j1Lk4Tw4o+Z+dQXbBX8CQmAjXgP3Qo8Pw/jq157SioIVfWXO/rfQ6SdVZpgTkDZETNAi1/RMsgzC/TGCv0JLESP9Z8mYvRVKs8Mp1Dx3bEF2/G6tEjgFQWwsAOLrXcAg27YfzVAD+5FSBEDjoSt3YUiVrDM2yFiQJc0pKt847d8kREmOz+MT5ffV3tV/Yy3lNHbysUEnTIpXzU+w2lFrkb2yTBgfabeXVErs+q7y8pISXE6BPW96D09+gbZTR0eHixU2UqMgfUq9NCCt7btUzpXd9Q+9VEcgIAV7zGrZv0eL1LUrVhMWXIhcoKyjF/KCVcz6bWrevkhbauirjC5u2M2khQlpf4XMVtHk/i0snuFD692cilLQLU0= home@Yassers-MacBook-Pro.local"
+}
+
+variable "device_name" {
+   type = string
+   default =   "/dev/sdf"
+}
+
+variable "availability_zone" {
+   type = string
+   default = "eu-west-1a"
+}
+
+variable "size" {
+   type = string
+   default = "50"
+}
+
+variable "ebs_type" {
+   type = string
+   default = "gp2"
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    "platform" : "kubernetes"
+  }
+}
+
+
+
+
+
+
